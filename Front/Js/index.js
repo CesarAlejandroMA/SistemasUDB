54f2433dc08c6dfe796e1592f4bd1eceea5418ef
CesarAlejandroMA <125744912+CesarAlejandroMA@users.noreply.github.com>
2024-12-03 21:17:47 -0500

    Update index.js
    Cambio a url camax

--- Front/Js/index.js
+++ Front/Js/index.js
@@ -17,7 +17,7 @@ document.getElementById('emailForm').addEventListener('submit', function(event) 
     //
 
     // Enviar el correo al backend para generar y enviar el código
-    fetch('https://c00b-181-55-70-113.ngrok-free.app/send-verification-code', {
+    fetch('https://9816-186-114-123-249.ngrok-free.app/send-verification-code', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
